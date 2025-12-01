9bea9cc34cc82a8ff0963600ec76308dd482ca81
Emma <edonosov01@educarex.es>
2025-12-01 14:26:56 +0100

    multiplicar

--- src/app/botonesnumeros/tablamultiplicar/tablamultiplicar.ts
+++ src/app/botonesnumeros/tablamultiplicar/tablamultiplicar.ts
@@ -8,14 +8,19 @@ import { Component } from '@angular/core';
 })
 export class tablamultiplicar{
 
+  resultados:string[] = [];
   numero : number = 5;
+
+
 imprimirtablam(){
    let numero = this.numero ;
-   let resultado : String ="";
    let multiplicar = 0;
+   let resultado1;
+
   for(let index =0; index <=0; index++){
     multiplicar= 5*index;
-    resultado = numero + "x" + index + "=" + multiplicar;
+    resultado1 = "·" + numero + "x" + index + "=" + multiplicar + "·";
+    this.resultados.push(resultado1);
   }
 }
 }
